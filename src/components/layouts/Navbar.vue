7d6e6ce863c4cbd26361cfab9b63311fb259c37a
Priyanku <hazarikapriyanku94@gmail.com>
2020-03-08 01:12:53 +0530

    Maps loaded

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+        <nav class="blue darken-2">
+            <div class="container">
+                <a href="" class="brand-logo left">Geo Chat</a>
+                <ul class="right">
+                    <li><a href="">SignUp</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
